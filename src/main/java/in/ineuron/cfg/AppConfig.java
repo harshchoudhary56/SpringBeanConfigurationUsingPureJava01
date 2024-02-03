8f5e0e188426dd96f44dafef3efdcdb393a64a84
Harsh <sample@gmail.com>
2024-02-03 23:35:20 +0530

    first commit

---
+++ src/main/java/in/ineuron/cfg/AppConfig.java
@@ -0,0 +1,26 @@
+package in.ineuron.cfg;
+
+import java.time.LocalDateTime;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "in.ineuron" })
+public class AppConfig {
+	
+	static {
+		System.out.println("AppConfig.class file is loading...");
+	}
+	
+	public AppConfig() {
+		System.out.println("AppConfig :: zero param constructor");
+	}
+	
+	@Bean(value = "date")
+	public LocalDateTime createDateTime() {
+		System.out.println("AppConfig.createLocalDateTime()");
+		return LocalDateTime.now();
+	}
+}
